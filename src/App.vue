cc68e1986a7194192acec1ca538f2aad3ea4e315
Fiona Wong <wong0328@algonquinlive.com>
2023-12-11 13:26:52 -0500

    Fiona's contact book

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="wrapper">
-    <h1 class="app-title">Fiona Contact Book</h1>
+    <h1 class="app-title">Fiona's Contact Book</h1>
     <router-view />
   </div>
 </template>
